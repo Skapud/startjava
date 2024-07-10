2b063c9b13378098320497fecc644be88ec04afe
Игорь_Подолян <tinyxxl@bk.ru>
2024-07-10 16:49:49 +0400

    Сделал 3-е задание, модифицировал Wolf и WolfTest

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,27 +1,71 @@
 public class Wolf {
-    String gender;
-    String name;
-    int weight;
-    int age;
-    String colour;
+    private String gender;
+    private String name;
+    private int weight;
+    private int age;
+    private String colour;
 
-    void move() {
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            throw new IllegalArgumentException("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void move() {
         System.out.println("moving");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("sitting");
     }
 
-    void run() {
+    public void run() {
         System.out.println("running");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("howling");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("hunting");
     }
 }
--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,16 +1,16 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = "male";
-        wolf.name = "wolfy";
-        wolf.weight = 55;
-        wolf.age = 10;
-        wolf.colour = "Grey";
-        System.out.println("Пол - " + wolf.gender);
-        System.out.println("Имя - " + wolf.name);
-        System.out.println("Вес - " + wolf.weight);
-        System.out.println("Возраст - " + wolf.age);
-        System.out.println("Цвет - " + wolf.colour);
+        wolf.setGender("male");
+        wolf.setName("wolfy");
+        wolf.setWeight(55);
+        wolf.setAge(5);
+        wolf.setColour("Grey");
+        System.out.println("Пол - " + wolf.getGender());
+        System.out.println("Имя - " + wolf.getName());
+        System.out.println("Вес - " + wolf.getWeight());
+        System.out.println("Возраст - " + wolf.getAge());
+        System.out.println("Цвет - " + wolf.getColour());
         wolf.move();
         wolf.sit();
         wolf.run();

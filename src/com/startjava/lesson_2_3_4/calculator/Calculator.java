fc8a99ecdbab378425d04fa91d502c3d8217b965
Игорь_Подолян <tinyxxl@bk.ru>
2024-08-28 20:37:15 +0400

    Работа над ошибками в калькуляторе

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,92 +1,53 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.text.DecimalFormat;
-
 public class Calculator {
-    private String formula;
-    private double arg1;
+    private int arg1;
     private String mathSign;
-    private double arg2;
-    private double result;
-
-    public String getFormula() {
-        return formula;
-    }
-
-    public void setFormula(String formula) {
-        if (formula.isBlank()) {
-            throw new RuntimeException("Ошибка: строка является пустой или содержит только пробелы");
-        }
-        this.formula = formula;
-    }
+    private int arg2;
 
     public double getArg1() {
         return arg1;
-    }
-
-    public void setArg1(double arg1) {
-        this.arg1 = arg1;
     }
 
     public String getMathSign() {
         return mathSign;
     }
 
-    public void setMathSign(String mathSign) {
-        this.mathSign = mathSign;
-    }
-
     public double getArg2() {
         return arg2;
     }
 
-    public void setArg2(double arg2) {
-        this.arg2 = arg2;
-    }
-
-    public void checkFormulaParts(double arg1, double arg2, String mathSign) {
-        if (Double.isNaN(arg1) || Double.isNaN(arg2)) {
-            throw new RuntimeException("Ошибка: введено недопустимое число");
-        }
-        if (mathSign == null || mathSign.isBlank()) {
-            throw new RuntimeException("Ошибка: знак не может быть пустым или содержать пробелы");
-        }
-        if (!mathSign.equals("+") && !mathSign.equals("-") && !mathSign.equals("*") &&
-                !mathSign.equals("/") && !mathSign.equals("%") && !mathSign.equals("^")) {
-            throw new RuntimeException("Ошибка: операция '" + mathSign +
-                    "' не поддерживается." +
-                    "\nДоступны следующие операции: +, -, *, /, ^, %");
-        }
-    }
-
-    public double calculate() {
+    public double calculate(String formula) {
+        String[] formulaParts = formula.split(" ");
+        checkArg(formulaParts[0]);
+        checkArg(formulaParts[2]);
+        arg1 = Integer.parseInt(formulaParts[0]);
+        mathSign = formulaParts[1];
+        arg2 = Integer.parseInt(formulaParts[2]);
         switch (mathSign) {
             case "+":
-                result = arg1 + arg2;
-                break;
+                return arg1 + arg2;
             case "-":
-                result = arg1 - arg2;
-                break;
+                return arg1 - arg2;
             case "*":
-                result = arg1 * arg2;
-                break;
+                return arg1 * arg2;
             case "/":
             case "%":
                 if (arg2 == 0) {
                     throw new IllegalArgumentException("Ошибка: деление на ноль запрещено");
                 }
-                result = (mathSign.equals("/")) ? arg1 / arg2 : Math.IEEEremainder(arg1, arg2);
-                break;
+                return (mathSign.equals("/")) ?
+                        (double) arg1 / (double) arg2 : Math.IEEEremainder(arg1, arg2);
             case "^":
-                result = Math.pow(arg1, arg2);
-                break;
+                return Math.pow(arg1, arg2);
+            default:
+                return Double.NaN;
         }
-        return result;
     }
 
-    public void print(double arg1, double arg2, String mathSign, double result) {
-        DecimalFormat df = new DecimalFormat("#.###");
-        System.out.println(df.format(arg1) + " " + mathSign + " " +
-                    df.format(arg2) + " = " + df.format(result));
+    public void checkArg(String arg) {
+        if (arg == null || !arg.matches("-?\\d+")) {
+            throw new IllegalArgumentException("Ошибка: введено недопустимое число");
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,34 +1,25 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
-        String answer = "";
-        boolean shouldContinue = true;
-        while (true) {
-            if (shouldContinue) {
+        String answer = "yes";
+        do {
+            if (answer.equals("yes")) {
                 System.out.println("Введите математическое выражение: a ^ b");
-                calc.setFormula(scanner.nextLine());
-                String[] formulaParts = calc.getFormula().split(" ");
-                calc.setArg1(Integer.parseInt(formulaParts[0]));
-                calc.setMathSign(formulaParts[1]);
-                calc.setArg2(Integer.parseInt(formulaParts[2]));
-                calc.checkFormulaParts(calc.getArg1(), calc.getArg2(), calc.getMathSign());
-                calc.print(calc.getArg1(), calc.getArg2(), calc.getMathSign(), calc.calculate());
+                double result = calc.calculate(scanner.nextLine());
+                DecimalFormat df = new DecimalFormat("#.###");
+                System.out.println(df.format(calc.getArg1()) + " " + calc.getMathSign() + " " +
+                        df.format(calc.getArg2()) + " = " + df.format(result));
                 System.out.println("Хотите продолжить вычисления ? [yes/no]:");
-                shouldContinue = false;
-            } else if (answer.equals("no")) {
-                break;
-            } else if (answer.equals("yes")) {
-                shouldContinue = true;
-                continue;
             } else {
                 System.out.println("Введите корректный ответ [yes / no]:");
             }
             answer = scanner.nextLine().toLowerCase();
-        }
+        } while (!answer.equals("no"));
     }
 }
